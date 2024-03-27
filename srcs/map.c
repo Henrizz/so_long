8df61fb289b3c3528c87a0e9bebc8cbe9d224caa
Henriette Zimmermann <hzimmerm@c4c6c4.42berlin.de>
2024-03-27 18:44:52 +0100

    edit

--- srcs/map.c
+++ srcs/map.c
@@ -6,7 +6,7 @@
 /*   By: hzimmerm <hzimmerm@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 17:03:37 by hzimmerm          #+#    #+#             */
-/*   Updated: 2024/03/27 17:59:39 by hzimmerm         ###   ########.fr       */
+/*   Updated: 2024/03/27 18:43:15 by hzimmerm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	read_map(char *map, t_game *game)
 		exit(EXIT_FAILURE);
 	game->width = ft_strlen(read_m);
 	ft_printf("game width = %d", game->width);
-	game->height = 0;
+	game->height = 1;
 	ft_printf("game height = %d", game->height);
 	while (read_m)
 	{
@@ -40,6 +40,22 @@ int	read_map(char *map, t_game *game)
 		ft_printf("game height = %d", game->height);
 	}
 	close(fd);
+	write_check_map(game, game->height, map);
 	free(read_m);
 	return (0);
+}
+
+int	write_check_map(t_game *game, int height, char* map)
+{
+	int	i;
+	int	fd;
+
+	i = 0;
+	fd = open(map, O_RDONLY);
+	game->field = malloc(sizeof(char *) * height);
+	while (i < height)
+	{
+		game->field[i] = get_next_line(fd);
+		i++;
+	}
 }
